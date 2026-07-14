e42a547d1635850e754cb084e8f1df2c67daad2a
bulkabonya-design <bulkabonya@gmail.com>
2026-07-14 03:55:47 +0300

    Create MobDeathMod.java

---
+++ src/main/java/ru/bulkabonya/mobdeath/MobDeathMod.java
@@ -0,0 +1,10 @@
+package ru.bulkabonya.mobdeath;
+
+import net.fabricmc.api.ModInitializer;
+
+public class MobDeathMod implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        System.out.println("Мод от bulkabonya запущен!");
+    }
+}
